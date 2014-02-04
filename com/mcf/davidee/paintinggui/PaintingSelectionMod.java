4ae5ab4c968662776735695b12db504bd8adc282
Sylvain THESNIERES <garvek@gmail.com>
2014-02-04 01:09:33 +0100

    Update to 1.7.2

--- com/mcf/davidee/paintinggui/PaintingSelectionMod.java
+++ com/mcf/davidee/paintinggui/PaintingSelectionMod.java
@@ -1,73 +1,41 @@
 package com.mcf.davidee.paintinggui;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-import net.minecraft.command.ServerCommandManager;
-import net.minecraft.entity.item.EntityPainting;
-import net.minecraft.network.packet.Packet250CustomPayload;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.EnumArt;
-
-import com.mcf.davidee.paintinggui.forge.ClientPacketHandler;
-import com.mcf.davidee.paintinggui.forge.ServerPacketHandler;
-import com.mcf.davidee.paintinggui.gui.ArtComparator;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+import com.mcf.davidee.paintinggui.forge.PacketPipeline;
+import com.mcf.davidee.paintinggui.forge.ServerPacketHandler;
+import com.mcf.davidee.paintinggui.CommandPainting;
+import com.mcf.davidee.paintinggui.gui.ArtComparator;
 
 
-@Mod(modid = "PaintingSelGui", name = "PaintingSelectionGui", version = "1.6.4.0", dependencies = "after:guilib")
-@NetworkMod(
-		clientSideRequired = true,
-		serverSideRequired = false,
-		clientPacketHandlerSpec = @SidedPacketHandler(channels = "PaintingSelGui",packetHandler = ClientPacketHandler.class),
-		serverPacketHandlerSpec = @SidedPacketHandler(channels = "PaintingSelGui", packetHandler = ServerPacketHandler.class)
-		)
+@Mod(modid = "PaintingSelGui", name = "PaintingSelectionGui", version = "1.7.2.0", dependencies = "after:guilib")
 public class PaintingSelectionMod {
 	
-	public static final String CHANNEL = "PaintingSelGui";
-	
-	public static final char COLOR = '\u00A7';
-	
+    public static final PacketPipeline dispatcher = new PacketPipeline();
+    
 	public static final ArtComparator ART_COMPARATOR = new ArtComparator();
-	
-	public static EnumArt getEnumArt(String artName) {
-		for (EnumArt art : EnumArt.values())
-			if (art.title.equals(artName))
-				return art;
-		return EnumArt.Kebab;
-	}
-
-	public static void setPaintingArt(EntityPainting p, EnumArt art) {
-		p.art = art;
-		p.setDirection(p.hangingDirection);
-	}
-	
-	public static Packet250CustomPayload createPacket(int id, String[] art) {
-		try{
-			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-			DataOutputStream dos = new DataOutputStream(bytes);
-			dos.writeInt(id);
-			dos.writeInt(art.length);
-			for(String s : art)
-				dos.writeUTF(s);
-			return new Packet250CustomPayload(CHANNEL,bytes.toByteArray()); 
-		}
-		catch(IOException e){
-			return null;
-		}
-	}
+    
+    @SidedProxy(clientSide="com.mcf.davidee.paintinggui.forge.ClientPacketHandler", serverSide="com.mcf.davidee.paintinggui.forge.ServerPacketHandler")
+    public static ServerPacketHandler proxy;
+    
+    @EventHandler
+	public void initialize(FMLInitializationEvent init) {
+        dispatcher.initialize();
+        proxy.initialize();
+    }
+    
+    @EventHandler
+    public void postInitialize(FMLPostInitializationEvent evt) {
+        dispatcher.postInitialize();
+    }
 	
 	@EventHandler
 	public void serverStarting(FMLServerStartingEvent event) {
-		MinecraftServer server= event.getServer();
-		ServerCommandManager serverCommandManager = (ServerCommandManager) server.getCommandManager();
-		serverCommandManager.registerCommand(new CommandPainting());
+        event.registerServerCommand(new CommandPainting());
 	}
-	
 }
---
+++ com/mcf/davidee/paintinggui/forge/PaintingPacket.java
@@ -0,0 +1,67 @@
+package com.mcf.davidee.paintinggui.forge;
+
+import java.io.IOException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.channel.ChannelHandlerContext;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+import com.mcf.davidee.paintinggui.forge.IPacket;
+import com.mcf.davidee.paintinggui.PaintingSelectionMod;
+
+
+public class PaintingPacket implements IPacket {
+    public int id;
+    public String[] art;
+    
+    public PaintingPacket() {
+        id = -1;
+        art = new String[0];
+    }
+    
+    public PaintingPacket(int theId, String[] theArt) {
+        id = theId;
+        art = new String[theArt.length];
+        for (int j=0; j<theArt.length; j++) {
+            art[j] = theArt[j];
+        }
+    }
+    
+    public void decodeInto(ChannelHandlerContext ctx, ByteBuf buffer) {
+        ByteBufInputStream dis = new ByteBufInputStream(buffer);
+        try {
+            id = dis.readInt();
+            art = new String[dis.readInt()];
+            for (int i = 0; i < art.length; ++i)
+                art[i] = dis.readUTF();
+        }
+        catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+    
+    public void encodeInto(ChannelHandlerContext ctx, ByteBuf buffer) {
+        System.out.println("[Painting GUI] PaintingPacket.writeBytes");
+        try {
+            dos.writeInt(id);
+            dos.writeInt(art.length);
+            for(String s : art)
+                dos.writeUTF(s);
+        }
+        catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+    
+    public void handleClientSide(EntityPlayer player) {
+        PaintingSelectionMod.proxy.onClientPacket(player, this);
+    }
+    
+    public void handleServerSide(EntityPlayer player) {
+        PaintingSelectionMod.proxy.onServerPacket((EntityPlayerMP)player, this);
+    }
+}
